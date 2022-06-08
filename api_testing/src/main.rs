0335c4715476f90de91bf18bbe73346bbea33dd1
brad90four <42116429+brad90four@users.noreply.github.com>
2022-06-08 15:13:51 -0400

    implement basic parsing for latlon

--- api_testing/src/main.rs
+++ api_testing/src/main.rs
@@ -1,6 +1,7 @@
 // api_test in rust
 pub mod data_struct;
 
+use clap::Parser;
 use dotenv::dotenv;
 use reqwest::Error;
 use std::env;
@@ -9,17 +10,30 @@ use serde_json::Value;
 
 use crate::data_struct::OpenWeatherMapResponse;
 
+#[derive(Debug, Parser)]
+struct Args {
+    // Latitude to use for the request
+    #[clap(short = 'a', long = "lat", default_value = 40.7128)]
+    lat: f64,
+    // Longitude to use for the request
+    #[clap(short = 'o', long = "lon", default_value = 74.0060)]
+    lon: f64
+}
+
 #[tokio::main] // used for async calls
 async fn main() -> Result<(), Error> {
     dotenv().expect(".env file not found");
 
     let api_key = env::var("API_KEY").expect("API_KEY not found");
+    let args = Args::parse();
+    let lat = args.lat.to_string();
+    let lon = args.lon.to_string();
 
     let request_url = format!(
         "https://api.openweathermap.org/data/2.5/weather?\
             lat={lat}&lon={lon}&appid={API_KEY}&units={units}",
-        lat = "0",
-        lon = "0",
+        lat = lat,
+        lon = lon,
         API_KEY = api_key,
         units = "imperial"
     );
